4aac770281d33b7bebe151e97ff9a7ff6f0a41e6
Ruramai <95416939+RuramaiB@users.noreply.github.com>
2023-11-03 03:40:29 -0700

    Update locals.vue added prefix

--- pages/superAdmin/locals.vue
+++ pages/superAdmin/locals.vue
@@ -65,6 +65,9 @@
                                   Location
                                 </th>
                                 <th scope="col" class="px-6 py-3">
+                                  Prefix
+                                </th>
+                                <th scope="col" class="px-6 py-3">
                                     
                                 </th>
                                 <th scope="col" class="px-6 py-3"></th>
@@ -75,6 +78,7 @@
                               <td class="px-6 py-4">{{ item.id }}</td>
                               <td class="px-6 py-4">{{ item.name }}</td>
                               <td class="px-6 py-4">{{ item.location }}</td>
+                            <td class="px-6 py-4">{{ item.prefix }}</td>
                               <td class="px-3 py-4">
                                 <a href="#" class="font-medium text-green-500 dark:text-blue-500 hover:underline">
                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  @click="getLocalByName(item.name)"  class="bi bi-pencil-square" viewBox="0 0 16 16"> <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/> <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/> </svg>
